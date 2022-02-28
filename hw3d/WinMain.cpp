497c72f5c303740a3971cf0fde1fd4f4f5039f3d
SilVerC77 <silverc777@hotmail.com>
2022-03-01 02:53:29 +0900

    Message Loop/WndProc

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,6 +1,63 @@
 #include <Windows.h>
+#include <tchar.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow) {
-	while (true);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg) {
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+INT CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	const auto pClassName = _T("first DirectX");
+	//register windwo class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	RegisterClassEx(&wc);
+
+	//create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		_T("Happy DirectX"),
+		WS_OVERLAPPEDWINDOW,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	//message
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1) {
+		return -1;
+	}
+	else {
+		return msg.wParam;
+	}
+
 	return 0;
 }
