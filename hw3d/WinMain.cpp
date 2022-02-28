808fc82730a4d24d0c6ba1d534ead42bb8b3a7e0
SilVerC77 <silverc777@hotmail.com>
2022-03-01 03:10:31 +0900

    WindowMessage

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -7,6 +7,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	case WM_CLOSE:
 		PostQuitMessage(69);
 		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, _T("GG.COM"));
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, _T("YYDS"));
+		}
+		break;
 	}
 
 	return DefWindowProc(hWnd, msg, wParam, lParam);
@@ -14,8 +24,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 INT CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	const auto pClassName = _T("first DirectX");
 	//register windwo class
+	const auto pClassName = _T("first DirectX");
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_HREDRAW | CS_VREDRAW;
